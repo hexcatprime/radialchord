669d46e49ec5d138bdf7ef8ec210e5edcb68f3fc
dubsbol <sambeequick@gmail.com>
2024-01-28 10:01:21 -0500

    virtual mouse support, change to polling from event drived

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use enigo::{keycodes::Key::*, *};
 use gilrs::{Gamepad, Gilrs, Event, Button, EventType};
 use std::{collections::HashMap, f32::consts::PI};
 
+const MOUSE_MULTIPLIER: f32 = 10.0;
 struct Joystick
 {
     zone_angle: f32,
@@ -35,7 +36,7 @@ impl Joystick
     }
     pub fn print(&self)
     {
-        print!("\rAxes: ({:+05.3},{:+05.3})\tAngle: ({:+08.3})\tZone: ({:2}, \tActive?: ({})", self.axis_x,self.axis_y,self.angle, self.zone, self.active);
+        print!("\rAxes: ({:+05.3},{:+05.3})\tAngle: ({:+08.3})\tZone: ({:2}, \tActive?: ({})", self.axis_x as i32,self.axis_y as i32,self.angle, self.zone, self.active);
     }
 }
 
@@ -52,6 +53,7 @@ fn main()
     let mut cached_key: Key = Layout('0');
 
     let mut zone_lock: bool = false;
+    let mut context_lock: bool = false;
 
     loop 
     {
@@ -64,12 +66,13 @@ fn main()
                 {
                     match key_map.get(&button)
                     {
-                        Some(&F34) => enigo.mouse_click(MouseButton::Left),
-                        Some(&F35) => enigo.mouse_click(MouseButton::Right),
+                        Some(&F33) => enigo.mouse_click(MouseButton::Left),
+                        Some(&F34) => enigo.mouse_click(MouseButton::Right),
+                        Some(&F35) => context_lock = !context_lock,
                         Some(&value) => enigo.key_click(Key::try_from(value).unwrap()),
                         _ => (),
                     }
-                    println!("{:?}", button);
+                    // println!("{:?}, {:?}", button, context_lock);
                 }
 
                 EventType::AxisChanged(..) =>
@@ -77,14 +80,17 @@ fn main()
                 active_gamepad = gilrs.gamepad(id);
                 stick_chord.set(active_gamepad.value(gilrs::Axis::LeftStickX), active_gamepad.value(gilrs::Axis::LeftStickY));
                 stick_note.set(active_gamepad.value(gilrs::Axis::RightStickX), active_gamepad.value(gilrs::Axis::RightStickY));
-
-                if stick_note.active
+                stick_chord.print();
+                if context_lock
+                {
+                    enigo.mouse_move_relative((stick_chord.axis_x * MOUSE_MULTIPLIER) as i32, -(stick_chord.axis_y * MOUSE_MULTIPLIER) as i32);
+                }
+                else if stick_note.active
                 {
                     cached_key = chord_map[stick_chord.zone as usize][stick_note.zone as usize];
                     zone_lock = true;
-                }
-    
-                if !stick_note.active && zone_lock
+                } 
+                else if zone_lock
                 {
                     enigo.key_click(cached_key);
                     zone_lock = false;
@@ -92,9 +98,6 @@ fn main()
                 }
                 _ => (),
             }
-            
-            // stick_note.print();
-            // println!("{:?}", cached_key);
         }
     }
 }
@@ -107,15 +110,17 @@ fn build_key_map() -> HashMap<Button, Key>
     temp.insert(Button::North, PageDown);
     temp.insert(Button::West, PageUp);
     temp.insert(Button::LeftTrigger, Control);
-    temp.insert(Button::LeftTrigger2, F35);
+    temp.insert(Button::LeftTrigger2, F34);
     temp.insert(Button::RightTrigger, Alt);
-    temp.insert(Button::RightTrigger2, F34);
+    temp.insert(Button::RightTrigger2, F33);
     temp.insert(Button::Select, Tab);
     temp.insert(Button::Start, Escape);
     temp.insert(Button::DPadUp, UpArrow);
     temp.insert(Button::DPadDown, DownArrow);
     temp.insert(Button::DPadLeft, LeftArrow);
     temp.insert(Button::DPadRight, RightArrow);
+    temp.insert(Button::LeftThumb, F35);
+    temp.insert(Button::RightThumb, Shift);
     
     temp
 }
